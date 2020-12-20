3cc73ea33ed5ca4af722ba3ad2152fdd101edcf8
koksal68 <74979941+koksal68@users.noreply.github.com>
2020-12-20 19:54:16 +0300

    Add files via upload

---
+++ soru1.py
@@ -0,0 +1,23 @@
+﻿import re
+
+def hepsiASCIIMi(dizge):
+    return all(ord(k) < 128 for k in dizge)
+
+def ePostaGeçerliMi(adres):
+    if re.match("\A(?P<name>[\w\-_]+)@(?P<domain>[\w\-_]+).(?P<toplevel>[\w]+)\Z", adres, re.IGNORECASE):
+        if (hepsiASCIIMi(adres)):
+            return True
+        else:
+            return False
+    return False
+
+def main():
+    email = input ("Lütfen bir e posta adresi giriniz: ")
+
+    if (ePostaGeçerliMi(email)):
+        print('Girilen adres geçerli')
+    else:
+        print('Girilen adres geçersiz')
+
+if __name__ == "__main__":
+    main()
---
+++ soru2.py
@@ -0,0 +1,13 @@
+﻿import re
+
+exp = '^(http[s]?:\/\/)?([w]{3}\.)?[a-z0-9\._-]+[.]\w{2,3}([.]\w{2})?$'
+
+
+def validateURL(url):
+    if (re.search(exp, url)):
+        print("URL doğrulandı")
+    else:
+        print("URL hatalı!")
+
+
+validateURL("www.alierbey.com")
